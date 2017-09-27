02ea80944aea233ea2336e8519f802d62fdefd3b
cunhasb CUNHA <cunhasb@aol.com>
2017-09-27 18:36:48 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,9 @@
 require 'pry'
 def alphabetize(arr)
   # code here
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.index(ESPERANTO_ALPHABET)
+  end  
   binding.pry
 end
