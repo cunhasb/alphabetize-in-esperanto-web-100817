5a88077f73c83d36df4d6665463fd08d4c9fa7c4
cunhasb CUNHA <cunhasb@aol.com>
2017-09-27 18:44:03 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,11 @@ def alphabetize(arr)
   # code here
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |phrase|
-    phrase.index(ESPERANTO_ALPHABET)
+    phrases.map |words| do
+      words.map |letters| do
+        ESPERANTO_ALPHABET.index(letters)
+      end
+    end
    binding.pry
   end
 end
